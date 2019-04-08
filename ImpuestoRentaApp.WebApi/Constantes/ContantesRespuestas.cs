3531056c67bed16fb4a5882a646afb0f6ad30ca5
victor soto <victor.soto.droguett@gmail.com>
2019-04-08 20:27:07 -0300

    007 - cambios webapi -  codigos de respuesta y control de respuestas

---
+++ ImpuestoRentaApp.WebApi/Constantes/ContantesRespuestas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ImpuestoRentaApp.WebApi.Constantes
+{
+    public class ContantesRespuestas
+    {
+        //Respuestas WebApi
+
+        public const string RespuestaOk = "01";
+        public const string RespuestaError = "03";
+        public const string RespuestaErrorControlado = "02";
+
+    }
+}
--- ImpuestoRentaApp.WebApi/Controllers/UsuariosController.cs
+++ ImpuestoRentaApp.WebApi/Controllers/UsuariosController.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using ImpuestoRentaApp.WebApi.Models;
+using ImpuestoRentaApp.WebApi.Constantes;
 
 namespace ImpuestoRentaApp.WebApi.Controllers
 {
@@ -17,13 +18,26 @@ namespace ImpuestoRentaApp.WebApi.Controllers
         /// Obtiene lista de usuarios
         /// </summary>
         /// <returns></returns>
-        [System.Web.Http.AcceptVerbs("GET", "POST")]
+        [Route("api/usuarios/ObtenerListaUsuarios")]
         [HttpGet]
         public IHttpActionResult ObtenerListaUsuarios()
         {
-            List<WinformApp.ImpuestoRentaAppDAL.BO.UsuarioBO> listaUsuarios = new List<WinformApp.ImpuestoRentaAppDAL.BO.UsuarioBO>();
-            listaUsuarios = usuarioBll.ObtenerListaUsuario();
-            return Ok(listaUsuarios);
+            try
+            {
+                List<WinformApp.ImpuestoRentaAppDAL.BO.UsuarioBO> listaUsuarios = new List<WinformApp.ImpuestoRentaAppDAL.BO.UsuarioBO>();
+                listaUsuarios = usuarioBll.ObtenerListaUsuario();
+                if (listaUsuarios.Count > 0)
+                {
+                    return Content(HttpStatusCode.Accepted,listaUsuarios);
+                }
+                else
+                {
+                    return Content(HttpStatusCode.NoContent,"no se encontraron resultados");
+                }
+            }catch(Exception ex)
+            {
+                return Content (HttpStatusCode.BadRequest, ex.Message);
+            }
         }
 
         /// <summary>
@@ -32,24 +46,43 @@ namespace ImpuestoRentaApp.WebApi.Controllers
         /// <param name="rut"></param>
         /// <returns></returns>
         [HttpGet]
-        [Route("api/usuarios/ObtenerUsuarioPorRut")]
+        [Route("api/usuarios/ObtenerUsuarioPorRut/{rut}")]
         public IHttpActionResult ObtenerUsuarioPorRut(string rut)
         {
             WinformApp.ImpuestoRentaAppDAL.BO.UsuarioBO usuarioBo = new WinformApp.ImpuestoRentaAppDAL.BO.UsuarioBO();
             WinformApp.ImpuestoRentaAppDAL.Usuario usuario = new WinformApp.ImpuestoRentaAppDAL.Usuario();
-            usuario = usuarioBll.ObtenerUsuarioPorRut(rut);
-
-            usuarioBo = new WinformApp.ImpuestoRentaAppDAL.BO.UsuarioBO()
+            usuarioBo = null;
+            try
             {
-                NombreUsuario = usuario.nombre_usuario,
-                CorreoElectronico = usuario.correo_electronico,
-                EstaActivo = usuario.esta_activo,
-                DescripcionEstaActivo = usuario.esta_activo ==true? "SI" : "NO",
-                Rut = usuario.rut,
-                Clave = usuario.clave
-            };
+                usuario = usuarioBll.ObtenerUsuarioPorRut(rut);
 
-            return Ok(usuarioBo);
+                if (usuario != null)
+                {
+                    usuarioBo = new WinformApp.ImpuestoRentaAppDAL.BO.UsuarioBO()
+                    {
+                        NombreUsuario = usuario.nombre_usuario,
+                        CorreoElectronico = usuario.correo_electronico,
+                        EstaActivo = usuario.esta_activo,
+                        DescripcionEstaActivo = usuario.esta_activo == true ? "SI" : "NO",
+                        Rut = usuario.rut,
+                        Clave = usuario.clave
+                    };
+
+                    return Content(HttpStatusCode.Accepted, usuarioBo);
+                }
+                else
+                {
+                    return Content(HttpStatusCode.NoContent,"No se encontraron resultados");
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return Content(HttpStatusCode.BadRequest, ex.Message);
+
+            }
+
         }
     }
 }
